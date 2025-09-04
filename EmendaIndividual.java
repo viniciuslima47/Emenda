a7ea88c2d705290214b983ebb62644a8ec8d63d8
Vinícius Lima <viniciuslucena303@gmail.com>
2025-09-04 00:19:35 -0300

    Delete EmendaIndividual.java; Delete EmendaRelator.java

--- EmendaIndividual.java
+++
@@ -1,8 +0,0 @@
-public class EmendaIndividual extends Emenda {
-    private String tipoTransferencia; // "Definida" ou "PIX"
-
-    public EmendaIndividual(int id, String autor, int ano, String tipoTransferencia) {
-        super(id, autor, ano);
-        this.tipoTransferencia = tipoTransferencia;
-    }
-}
